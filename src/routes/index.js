6426d93d094b3af2305d4279c24fbdcf9b38316f
thanh-do28 <thanhdo280795@gmail.com>
2023-05-05 10:34:08 +0700

    add

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import auth from "./auth"
 
 const initRoutes = (app) => {
     app.use("/api/v1/user", user)
-    app.use("/api/v1/auth", user)
+    app.use("/api/v1/auth", auth)
 
     return app.use("/", (req,res) => {
         return res.send("sever on")
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,11 +1,11 @@
-import db from "../models";
+
 
 export const register = () => new Promise((resolve, reject) => {
     try {
-        resolve("register service")
-        console.log("after resolve");
-
-        console.log(123);
+        resolve({
+            err: 0,
+            mes: "register service"
+        })
     } catch (error) {
         reject(error)
     }
